d8a684784c1fe84e4c68e29304e5cca89611b773
webpig <11765461@qq.com>
2020-01-12 13:03:38 +0800

    feat: factory pattern

---
+++ src/Factory/CaliforniaPizzaStore.java
@@ -0,0 +1,16 @@
+package Factory;
+
+public class CaliforniaPizzaStore extends PizzaStore {
+    public Pizza createPizza(String item) {
+//        if (item.equals("cheese")) {
+//            return new CaliforniaStyleCheesePizza();
+//        } else if (item.equals("veggie")) {
+//            return new CaliforniaStyleVeggiePizza();
+//        } else if (item.equals("clam")) {
+//            return new CaliforniaStyleClamPizza();
+//        } else if (item.equals("pepperoni")) {
+//            return new CaliforniaStylePepperoniPizza();
+//        } else return null;
+        return null;
+    }
+}
